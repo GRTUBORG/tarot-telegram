7c4abbd08f382f4712032cb913f216cbb49c6118
Dennis <blinhic850@mail.ru>
2022-07-09 03:21:48 +0300

    Update tarot.py

--- tarot.py
+++ tarot.py
@@ -9,7 +9,7 @@ from random import randint
 
 help_msg = """Приветствую, путник! Вот список моих магических команд:
 • /question — задать мне вопрос;
-• /help — вызвать это меню.
+• /help_ — вызвать это меню.
 
 По поводу платных раскладов писать @whomet."""
 
@@ -24,7 +24,7 @@ def welcome(message):
     keyboard.add(button)
     bot.send_message(message.chat.id, f"Привет, @{message.chat.username}! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
 
-@bot.message_handler(commands = ['help'])
+@bot.message_handler(commands = ['help_'])
 def help(message):
     bot.reply_to(message, help_msg)
 
