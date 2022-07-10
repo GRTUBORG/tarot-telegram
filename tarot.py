77bf7bb4421ee6676eb6fec25a2201e139448631
Dennis <blinhic850@mail.ru>
2022-07-10 19:12:32 +0300

    Update tarot.py

--- tarot.py
+++ tarot.py
@@ -36,6 +36,8 @@ def welcome(message):
         bot.send_message(message.chat.id, f"Привет, @{message.chat.username}! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
     else:
         bot.send_message(message.chat.id, f"Привет, `{message.chat.first_name}`! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
+     
+    bot.send_message(655041562, f'У тебя +1 новый пользователь! \n{str_countes}')
 @bot.message_handler(commands = ['help_'])
 def help(message):
     bot.reply_to(message, help_msg)
