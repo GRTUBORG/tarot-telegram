e3bf73812cbd14374e724d9577b8244de2b7798d
Dennis <blinhic850@mail.ru>
2022-07-13 03:09:03 +0300

    Update tarot.py

--- tarot.py
+++ tarot.py
@@ -18,34 +18,65 @@ token = os.environ.get('bot_token')
 bot = telebot.TeleBot(str(token))
 print('Бот работает!')
 
-@bot.message_handler(commands = ['start'])
-def welcome(message):
-    str_countes = ''
-    countes = [f'`{message.from_user.id}` — ID,\n',
-               f'{message.from_user.first_name} — имя,\n',
-               f'{message.from_user.last_name} — фамилия,\n',
-               f'@{message.from_user.username} — username.'
-              ]
-    for x in countes:
-        str_countes += x
-    
-    keyboard = types.InlineKeyboardMarkup()
-    button = types.InlineKeyboardButton(text = "Задать мне вопрос", callback_data = 'question_callback')
-    keyboard.add(button)
-    if message.chat.username != None:
-        bot.send_message(message.chat.id, f"Привет, @{message.chat.username}! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
+@bot.message_handler(commands=['start'])
+def start(message):
+    user_id = message.chat.id
+    my_channel_id = -1001518496730
+    statuss = ['creator', 'administrator', 'member']
+    for i in statuss:
+        if i == bot.get_chat_member(chat_id=my_channel_id, user_id=message.from_user.id).status:
+            keyboard = types.InlineKeyboardMarkup()
+            button = types.InlineKeyboardButton(text = "Задать мне вопрос", callback_data = 'question_callback')
+            keyboard.add(button)
+            if message.chat.username != None:
+                bot.send_message(message.chat.id, f"Привет, @{message.chat.username}! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
+            else:
+                bot.send_message(message.chat.id, f"Привет, `{message.chat.first_name}`! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
+            break
     else:
-        bot.send_message(message.chat.id, f"Привет, `{message.chat.first_name}`! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
-    bot.send_message(655041562, f'У тебя +1 новый пользователь! \n\n{str_countes}', parse_mode = 'Markdown')
+        keyboard = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(text = "Подписаться ↗️", url = 'https://t.me/tarot_damn')
+        done = types.InlineKeyboardButton(text = "Я подписался ✅", callback_data = 'done')
+        keyboard.row(button)
+        keyboard.row(done)
+        bot.send_message(message.chat.id, "Подпишись на канал *«Личный советник»* для продолжения", parse_mode = 'Markdown', reply_markup = keyboard)
 
 @bot.message_handler(commands = ['help_'])
 def help(message):
-    bot.reply_to(message, help_msg)
+    user_id = message.chat.id
+    my_channel_id = -1001518496730
+    statuss = ['creator', 'administrator', 'member']
+    for i in statuss:
+        if i == bot.get_chat_member(chat_id=my_channel_id, user_id=message.from_user.id).status:
+            bot.reply_to(message, help_msg)
+            break
+    else:
+        keyboard = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(text = "Подписаться ↗️", url = 'https://t.me/tarot_damn')
+        done = types.InlineKeyboardButton(text = "Я подписался ✅", callback_data = 'done')
+        keyboard.row(button)
+        keyboard.row(done)
+        bot.send_message(message.chat.id, "Подпишись на канал *«Личный советник»* для продолжения", parse_mode = 'Markdown', reply_markup = keyboard)
+    
 
 @bot.message_handler(commands = ['question'])
 def question(message):
-    bot.send_message(message.chat.id, "*Напиши свой вопрос, а я дам тебе на него ответ...*", parse_mode = 'Markdown')
-    bot.register_next_step_handler(message, get_message)
+    user_id = message.chat.id
+    my_channel_id = -1001518496730
+    statuss = ['creator', 'administrator', 'member']
+    for i in statuss:
+        if i == bot.get_chat_member(chat_id=my_channel_id, user_id=message.from_user.id).status:
+            bot.send_message(message.chat.id, "*Напиши свой вопрос, а я дам тебе на него ответ...*", parse_mode = 'Markdown')
+            bot.register_next_step_handler(message, get_message)    
+            break
+    else:
+        keyboard = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(text = "Подписаться ↗️", url = 'https://t.me/tarot_damn')
+        done = types.InlineKeyboardButton(text = "Я подписался ✅", callback_data = 'done')
+        keyboard.row(button)
+        keyboard.row(done)
+        bot.send_message(message.chat.id, "Подпишись на канал *«Личный советник»* для продолжения", parse_mode = 'Markdown', reply_markup = keyboard)
+    
 
 @bot.callback_query_handler(func = lambda call: True)
 def callback_inline(call):
@@ -53,6 +84,20 @@ def callback_inline(call):
         if call.data == 'question_callback':
             bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, text = "*Напиши свой вопрос, а я дам тебе на него ответ...*", parse_mode = 'Markdown')
             bot.register_next_step_handler(call.message, get_message)
+        elif call.data == 'done':
+            user_id = call.message.chat.id
+            my_channel_id = -1001518496730
+            statuss = ['creator', 'administrator', 'member']
+            for i in statuss:
+                if i == bot.get_chat_member(chat_id = my_channel_id, user_id = call.message.chat.id).status:
+                    keyboard = types.InlineKeyboardMarkup()
+                    button = types.InlineKeyboardButton(text = "Задать мне вопрос", callback_data = 'question_callback')
+                    keyboard.add(button)
+                    bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id, text = f"Привет, `{call.message.chat.username}`! Я бот, который раскладывает карты 24/7 и способен ответить на твой любой вопрос в формате `да/нет`. Задай мне подходящий вопрос и я определю твою судьбу в один миг!\n\n*Пример:* `Меня завтра позовут на собеседование?`", parse_mode = 'Markdown', reply_markup = keyboard)
+                    break
+            else:
+                bot.answer_callback_query(callback_query_id = call.id, show_alert = False, text = "Подписки на канал не обнаружено ❗️") 
+                        
 
 @bot.message_handler(commands = ['donate'])
 def donationalerts(message):
@@ -60,14 +105,27 @@ def donationalerts(message):
     bot.register_next_step_handler(message, get_message)
     
 def get_message(message):
-    id = message.text
-    if id[0] != "/":
-        yes = randint(0, 1)
-        if yes == 1:
-            question = "`Да`"
-        else:
-            question = "`Нет`"
-        bot.send_message(message.chat.id, f"Ответ карт: {question}\nИ помни — карты *никогда* не врут!\n\nЧтобы задать ещё вопрос — просто напиши его в чат.\nЕсли какие-то команды *не сработали* — вызови их второй раз. ", parse_mode = 'Markdown')
-        bot.register_next_step_handler(message, get_message)
+    user_id = message.chat.id
+    my_channel_id = -1001518496730
+    statuss = ['creator', 'administrator', 'member']
+    for i in statuss:
+        if i == bot.get_chat_member(chat_id=my_channel_id, user_id=message.from_user.id).status:
+            id = message.text
+            if id[0] != "/":
+                yes = randint(0, 1)
+                if yes == 1:
+                    question = "`Да`"
+                else:
+                    question = "`Нет`"
+                bot.send_message(message.chat.id, f"Ответ карт: {question}\nИ помни — карты *никогда* не врут!\n\nЧтобы задать ещё вопрос — просто напиши его в чат.\nЕсли какие-то команды *не сработали* — вызови их второй раз. ", parse_mode = 'Markdown')
+                bot.register_next_step_handler(message, get_message)    
+            break
+    else:
+        keyboard = types.InlineKeyboardMarkup()
+        button = types.InlineKeyboardButton(text = "Подписаться ↗️", url = 'https://t.me/tarot_damn')
+        done = types.InlineKeyboardButton(text = "Я подписался ✅", callback_data = 'done')
+        keyboard.row(button)
+        keyboard.row(done)
+        bot.send_message(message.chat.id, "Подпишись на канал *«Личный советник»* для продолжения", parse_mode = 'Markdown', reply_markup = keyboard)
         
 bot.polling(none_stop = True)
